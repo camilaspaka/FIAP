3aabe9372116ebb4b5b2e3f9eff05279a526b87e
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-10-27 18:17:00 -0300

    Update Variaveis.py

--- Capitulo2-Variaveis/Variaveis.py
+++ Capitulo2-Variaveis/Variaveis.py
@@ -2,3 +2,6 @@ nome="Humberto Delgado"
 empresa='FIAP'
 qtde_funcionarios=500
 mediaMensalidade=1050.50
+print(nome + " trabalha na empresa " + empresa)
+print("Possui: ", qtde_funcionarios, " funcionarios.")
+print("A média da mensalidade é de : " + str(mediaMensalidade))
