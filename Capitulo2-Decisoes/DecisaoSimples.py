d5b10e509307e567bf0cb39b740657879b3352bb
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-10-31 11:56:10 -0300

    Create DecisaoSimples.py

---
+++ Capitulo2-Decisoes/DecisaoSimples.py
@@ -0,0 +1,6 @@
+nome=input("Digite o nome: ")
+idade=int(input("Digite a idade: "))
+prioridade="NÃO"
+if idade>=65:
+    prioridade="SIM"
+print("O paciente " + nome + " possui atendimento prioritario? " + prioridade)
