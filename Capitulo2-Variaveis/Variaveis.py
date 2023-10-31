18d7ceb73b3e0ade67444c02f2c5e6c904164bd8
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-10-31 11:18:39 -0300

    Teste
    funcionamento completo, sem erros

--- Capitulo2-Variaveis/Variaveis.py
+++ Capitulo2-Variaveis/Variaveis.py
@@ -1,7 +1,14 @@
-nome="Humberto Delgado"
-empresa='FIAP'
-qtde_funcionarios=500
-mediaMensalidade=1050.50
+nome=input("Digite um funcionario: ")
+empresa=input("Digite a instituição: ")
+qtde_funcionarios=int(input("Digite a qtde de funcionarios: "))
+mediaMensalidade=float(input("Digite a média da mensalidade: "))
 print(nome + " trabalha na empresa " + empresa)
-print("Possui: ", qtde_funcionarios, " funcionarios.")
-print("A média da mensalidade é de : " + str(mediaMensalidade))
+print("Possui ", qtde_funcionarios, " funcionarios.")
+print("A média da mensalidade é de: " + str(mediaMensalidade))
+print("=========================Verifique os tipos de dados abaixo:==================")
+print("O tipo de dado da variavel [nome] é: ",type(nome))
+print("O tipo de dado da variavel [empresa] é: ",type(empresa))
+print("O tipo de dado da variavel [qtde_funcionarios] é: ",type(qtde_funcionarios))
+print("O tipo de dado da variavel [mediaMensalidade] é: ",type(mediaMensalidade))
+
+#excelente exercicio sinceramente
