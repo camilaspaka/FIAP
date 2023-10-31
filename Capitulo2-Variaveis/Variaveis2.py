2a9056d3a871c68b88d785c57eda49a79296b0a0
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-10-31 11:28:40 -0300

    Update Variaveis2.py

---
+++ Capitulo2-Variaveis/Variaveis2.py
@@ -0,0 +1,6 @@
+responsavel=input("Digite o nome do responsavel: ")
+funcionario=input("Digite o nome de funcionario: ")
+evento=input("Digite o nome do evento: ")
+valor=float(input("Digite o valor que será ressarcido: "))
+
+print("Declarado para o senhora " + responsavel + ", que o senhor " + funcionario + " esteve presente no evento " + evento + " e gastou o valor de R$ " + str(valor) + " com a entrada.")
