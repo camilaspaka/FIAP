16b113546f432b3fef78296c49936e64809fd5d5
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-11-01 11:28:49 -0300

    Update DecisoesEncadeada.py

---
+++ Capitulo2-Decisoes/DecisoesEncadeada.py
@@ -0,0 +1,19 @@
+nome=input("Digite o nome: ")
+idade=int(input("Digite a idade: "))
+doenca_infectocontagiosa=input("Suspeita de doença infectocontagiosa?").upper()
+if idade>=65:
+    print("Paciente COM prioridade")
+    if doenca_infectocontagiosa=="SIM"
+        print("Encaminhe o paciente para a sala Amarela")
+    elif doenca_infectocontagiosa=="NAO"
+        print("Encaminhe o paciente para a sala Branca")
+    else:
+        print("Resposta a suspeita de doença infectocontagiosa com SIM ou NAO")
+else:
+    print("Paciente SEM prioridade")
+    if doenca_infectocontagiosa=="SIM":
+        print("Encaminhe o paciente para a sala Amarela")
+    elif doenca_infectocontagiosa=="NAO":
+        print("Encaminhe o paciente para a sala Branca")
+    else:
+        print("Responda a suspeita de doença infectocontagiosa com SIM ou NAO")
