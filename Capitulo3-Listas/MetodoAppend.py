0b679b81fa36519bad698f0b06abb2d187e87925
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-11-01 20:38:34 -0300

    Create MetodoAppend.py

---
+++ Capitulo3-Listas/MetodoAppend.py
@@ -0,0 +1,8 @@
+inventario=[]
+resposta="S"
+while resposta=="S":
+  inventario.append(input("Equipamento: "))
+  inventario.append(float(input("Valor: ")))
+  inventario.append(int(input("Número Serial: ")))
+  inventario.append(input("Departamento: "))
+  resposta=input("Digite "S" para continuar: ").upper()
