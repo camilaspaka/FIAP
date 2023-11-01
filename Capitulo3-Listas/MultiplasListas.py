7d232756c87384fbeab5e8329a62b2abbc347552
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-11-01 20:52:26 -0300

    Create MultiplasListas.py

---
+++ Capitulo3-Listas/MultiplasListas.py
@@ -0,0 +1,24 @@
+equipamentos = []
+valores = []
+seriais = []
+departamentos = []
+resposta = "S"
+while resposta == "S":
+  equipamentos.append(input("Equipamento: "))
+  valores.append(float(input("Valor: ")))
+  seriais.append(int(input("Número Serial: ")))
+  departamentos.append(input("Departamento: "))
+  resposta = input("Digite 'S' para continuar: ").upper()
+for indice in range(0,len(equipamentos)):
+  print("Equipamento..: ", (indice+1))
+  print("Nome.........: ", equipamentos[indice])
+  print("Valor........: ", valores[indice])
+  print("Serial.......: ", seriais[indice])
+  print("Departamento.: ", departamentos[indice])
+busca=input("Digite o nome do equipamento que deseja buscar: ")
+depreciacao=input("Digite o nome do equipamento que será depreciado: ")
+for indice in range(0,len(equipamentos)):
+  if depreciacao==equipamentos[indice]:
+    print("Valor antigo: ", valores[indice])
+    valores[indice] = valores[indice] * 0.9
+    print("Novo valor: ", valores[indice])
