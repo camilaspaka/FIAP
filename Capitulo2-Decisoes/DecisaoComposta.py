2672cca53c55e4c29e4ebdd8102140f4c6f11aac
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-11-01 11:05:47 -0300

    Update DecisaoComposta.py

--- Capitulo2-Decisoes/DecisaoComposta.py
+++ Capitulo2-Decisoes/DecisaoComposta.py
@@ -1,6 +1,9 @@
 nome=input("Digite o nome: ")
 idade=int(input("Digite a idade: "))
+doenca_infectocontagiosa=input("Suspeita de doença infecto-contagiosa?").upper()
 if idade>=65:
     print("O paciente " + nome + " possui atendimento prioritario!")
+elif doenca_infectocontagiosa=="SIM":
+    print("O paciente " + nome + " deve ser direcionado para a sala de espera reservada.")
 else:
-    print("O paciente " + nome + " não possui atendimento prioritário!")
+    print("O paciente" + nome + " NÂO possui atendimento prioritário e pode aguardar na sala comum ")
