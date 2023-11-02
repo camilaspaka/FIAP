3f1e437bd4aac43446ea3068f7b680f523287e86
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-11-01 21:21:27 -0300

    Update IdentificaoDeFuncoes.py

--- Capitulo3-Listas/IdentificaoDeFuncoes.py
+++ Capitulo3-Listas/IdentificaoDeFuncoes.py
@@ -1,9 +1,17 @@
 def preencherInventario(lista):
-  resp="S"
-  while resp == "S":
-    equipamento=[input("Equipamento: "),
-              float(input("Valor: ")),
-              int(input("Número Serial: ")),
-              input("Departamento: ")]
-    lista.append(equipamento)
-    resp = input("Digite 'S' para continuar: ").upper()
+    def preencherInventario(lista):
+        resp = "S"
+        while resp == "S":
+            equipamento = [input("Equipamento: "),
+                           float(input("Valor: ")),
+                           int(input("Número Serial: ")),
+                           input("Departamento: ")]
+            lista.append(equipamento)
+            resp = input("Digite 'S' para continuar: ").upper()
+
+    def exibirInventario(lista):
+        for elemento in lista:
+            print("Nome.........: ", elemento[0])
+            print("Valor........: ", elemento[1])
+            print("Serial.......: ", elemento[2])
+            print("Departamento.: ", elemento[3])
