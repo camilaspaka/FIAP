07dc9a22c659863cfcc1d9d2a1e9c8c6a4713ed9
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-11-01 21:22:39 -0300

    Update IdentificaoDeFuncoes.py

--- Capitulo3-Listas/IdentificaoDeFuncoes.py
+++ Capitulo3-Listas/IdentificaoDeFuncoes.py
@@ -15,3 +15,11 @@ def preencherInventario(lista):
             print("Valor........: ", elemento[1])
             print("Serial.......: ", elemento[2])
             print("Departamento.: ", elemento[3])
+
+def depreciarPorNome(lista, porc):
+  depreciacao=input("Digite o nome do equipamento que será depreciado: ")
+  for elemento in lista:
+    if depreciacao==elemento[0]:
+      print("Valor antigo: ", elemento[1])
+      elemento[1] = elemento[1] * (1-porc/100)
+      print("Novo valor: ", elemento[1])
