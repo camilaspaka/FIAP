25aebd5609c5285f5b8be573ea7b621aba60b55f
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-11-01 21:28:30 -0300

    funcoes
    aprendendo a utilizar funções

---
+++ Capitulo3-Listas/ModuloPrincipal.py
@@ -0,0 +1,19 @@
+from Capitulo3_Funcoes.IdentificacaoDeFuncoes import *
+
+minhaLista=[]
+print("Preenchendo")
+preencherInventario(minhaLista)
+print("Exibindo")
+exibirInventario(minhaLista)
+
+print("Pesquisando")
+localizarPorNome(minhaLista)
+print("Alterando")
+depreciarPorNome(minhaLista, 20)
+
+print("Excluindo")
+print(excluirPorSerial(minhaLista))
+exibirInventario(minhaLista)
+
+print("Resumindo")
+resumirValores(minhaLista)
