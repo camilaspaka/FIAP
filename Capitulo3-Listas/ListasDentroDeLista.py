d422a12f6cf07a5e22bae0daaafecc4aa6ed4ca0
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-11-01 21:07:19 -0300

    Update ListasDentroDeLista.py

--- Capitulo3-Listas/ListasDentroDeLista.py
+++ Capitulo3-Listas/ListasDentroDeLista.py
@@ -37,3 +37,11 @@ for elemento in inventario:
   print("Valor........: ", elemento[1])
   print("Serial.......: ", elemento[2])
   print("Departamento.: ", elemento[3])
+
+valores=[]
+for elemento in inventario:
+  valores.append(elemento[1])
+if len(valores)>0:
+  print("O equipamento mais caro custa: ", max(valores))
+  print("O equipamento mais barato custa: ", min(valores))
+  print("O total de equipamentos é de: ", sum(valores))
