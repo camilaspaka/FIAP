ecc64762dcf5f2c672162ac2ebfee1cc72410ae1
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-11-03 15:27:39 -0300

    Update Dicionarios1.py

--- Capitulo4-Dicionarios/Dicionarios1.py
+++ Capitulo4-Dicionarios/Dicionarios1.py
@@ -1,5 +1,10 @@
 usuarios={}
-usuarios={
-    "Chaves":["Chaves Silva","17/06/2017","Recep_01"],
-    "Quico":["Enrico Flores","03/06/2017","Raiox_02"]
-    }
+usuarios={"Chaves":["Chaves Silva","17/06/1975","Recep_01"],
+    "Quico":["Enrico Flores","03/06/1976","Raiox_02"],
+    "Quico":["Enrico Flores","03/06/1976","Raiox_03"] }
+usuarios["Florinda"]=["Florinda Flores", "26/11/2017", "Recep_01"]
+usuarios["Florinda"]=["Florinda Flores", "26/11/2016", "Recep_01"]
+
+print(usuarios)
+print("##############========#########")
+print("Dados: ",usuarios.get("Chaves"))
