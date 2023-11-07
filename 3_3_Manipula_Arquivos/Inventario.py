ade1c90368e44a85139215cacc6f750cea0fabeb
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-11-07 12:05:21 -0300

    Update Inventario.py

--- 3_3_Manipula_Arquivos/Inventario.py
+++ 3_3_Manipula_Arquivos/Inventario.py
@@ -1,11 +1,13 @@
-import Funcoes.Funcoes_Arquivos
+from  import *
 inventario={}
-opcao=chamarMenu()
+opcao=chamarMenu()Funcoes.Funcoes_Arquivos
 while opcao>0 and opcao<4:
     if opcao==1:
         registrar(inventario)
     elif opcao==2:
         persistir(inventario)
     elif opcao==3:
-        print(exibir())
+        resultado = exibir()
+        for linha in resultado:
+            print(linha[2:-1])
     opcao = chamarMenu()
