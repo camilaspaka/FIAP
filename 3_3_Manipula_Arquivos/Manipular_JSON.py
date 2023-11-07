b35e2a96bf4dc8c3f0eabf455d76e6d137e05af5
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-11-07 17:29:25 -0300

    aplicando json nos ensinamentos

--- 3_3_Manipula_Arquivos/Manipular_JSON.py
+++ 3_3_Manipula_Arquivos/Manipular_JSON.py
@@ -1,29 +1,10 @@
-import json
-import os
-if os.path.exists("inventario_json.json"):
-    with open("inventario_json.json", "r") as arq_json:
-        inventario = json.load(arq_json)
-else:
-    inventario={}
-opcao=int(input("Digite: <1> para registrar ativo"
-                      "<2> para exibir ativos armazenados: "))
+from Funcoes.Funcoes_JSON import *
+
+inventario = ler_arquivo("inventario_json.json")
+opcao=chamarMenu()
 while opcao>0 and opcao<3:
     if opcao==1:
-        resp = "S"
-        while resp == "S":
-            inventario[input("Digite o número patrimonial: ")] = [
-                input("Digite a data da última atualização: "),
-                input("Digite a descrição: "),
-		 input("Digite o departamento: ")]
-            resp = input("Digite <S> para continuar.").upper()
-        with open("inventario_json.json", "w") as arq_json:
-            json.dump(inventario, arq_json)
-        print("JSON gerado!!!!")
+        print(registrar(inventario, "inventario_json.json"))
     elif opcao==2:
-            for chave, dado in inventario.items():
-                print("Data.........: ", dado[0])
-                print("Descrição....: ", dado[1])
-                print("Departamento.: ", dado[2])
-    opcao = int(input("Digite: "
-                      "<1> para registrar ativo"
-                      "<2> para exibir ativos armazenados: "))
+        exibir("inventario_json.json")
+    opcao = chamarMenu()
