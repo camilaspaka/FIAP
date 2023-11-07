ecd5f762a3a4feb285d666b8f35b65a693ca819a
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-11-07 10:59:39 -0300

    Update Funcoes.py

--- Capitulo4-Dicionarios/Funcoes.py
+++ Capitulo4-Dicionarios/Funcoes.py
@@ -1,6 +1,16 @@
-usuarios={}
-opcao=perguntar()
-while opcao=="I" or opcao=="P" or opcao=="E" or opcao=="L":
-    if opcao=="I":
-        inserir(usuarios)
-    opcao = perguntar()
+def perguntar():
+    resposta = input("O que deseja realizar?
+" +
+                  "<I> - Para Inserir um usuário
+" +
+                  "<P> - Para Pesquisar um usuário
+" +
+                  "<E> - Para Excluir um usuário
+" +
+                  "<L> - Para Listar um usuário: ").upper()
+    return resposta
+
+def inserir(dicionario):
+    dicionario[input("Digite o login: ").upper()] = [input("Digite o nome: ").upper(),
+                                          input("Digite a última data de acesso: "),
+                                          input("Qual a última estação acessada: ").upper()]
