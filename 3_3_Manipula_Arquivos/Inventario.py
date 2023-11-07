243e3fa54d779748359d76eb5d4788da1c134293
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-11-07 11:53:17 -0300

    Update Inventario.py

--- 3_3_Manipula_Arquivos/Inventario.py
+++ 3_3_Manipula_Arquivos/Inventario.py
@@ -1,27 +1,11 @@
+from Funcoes.Funcoes_Arquivos import *
 inventario={}
-opcao=int(input("Digite: "
-            "<1> para registrar ativo"
-            "<2> para persistir em arquivo"
-            "<3> para exibir ativos armazenados: "))
+opcao=chamarMenu()
 while opcao>0 and opcao<4:
     if opcao==1:
-        resp="S"
-        while resp=="S":
-            inventario[input("Digite o número patrimonial: ")]=[
-            input("Digite a data da última atualização: "),
-            input("Digite a descrição: "),
-            input("Digite o departamento: ")]
-            resp=input("Digite <S> para continuar.").upper()
+        registrar(inventario)
     elif opcao==2:
-        with open("inventario.csv", "a") as inv:
-            for chave, valor in inventario.items():
-                inv.write(chave + ";" + valor[0] + ";" +
-					valor[1] + ";" +valor[2]+" ")
-                print("Persistido com sucesso!")
+        persistir(inventario)
     elif opcao==3:
-        with open("inventario.csv", "r") as inv:
-            print(inv.readlines())
-    opcao=int(input("Digite: "
-            "<1> para registrar ativo"
-            "<2> para persistir em arquivo"
-            "<3> para exibir ativos armazenados: "))
+        print(exibir())
+    opcao = chamarMenu()
