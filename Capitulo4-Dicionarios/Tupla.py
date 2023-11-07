9bc37a55c5469dd1ae4c577904f5f567052bb69c
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-11-07 11:04:08 -0300

    Update Tupla.py

--- Capitulo4-Dicionarios/Tupla.py
+++ Capitulo4-Dicionarios/Tupla.py
@@ -4,3 +4,6 @@ while resp=="S":
     ips[(input("Digite os dois primeiros octetos: "),
        input("Digite os dois últimos octetos: "))]=input("Nome da máquina: ")
     resp=input("Digite <S> para continuar: ").upper()
+    print("Exibindo ip´s: ")
+    for ip in ips.keys():
+        print(ip[0] + "." + ip[1])
