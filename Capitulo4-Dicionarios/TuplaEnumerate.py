48c3fb626368da251e2ed3b81b8086b009c5af31
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-11-07 11:05:56 -0300

    Update TuplaEnumerate.py

--- Capitulo4-Dicionarios/TuplaEnumerate.py
+++ Capitulo4-Dicionarios/TuplaEnumerate.py
@@ -8,3 +8,8 @@ while resp=="S":
     for chave in range(0, len(tupla)):
         print("Email: ", tupla[chave][1])
         usuarios[tupla[chave]] = [input("Digite o nome"), input("Digite o nível")]
+    for chave, dado in usuarios.items():
+        print("Usuario.:", chave[0])
+        print("Email...: ", chave[1])
+        print("Nome....: ", dado[0])
+        print("Nível...: ", dado[1])
