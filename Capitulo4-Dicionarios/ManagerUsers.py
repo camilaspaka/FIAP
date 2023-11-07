2302f19f4474a8b2846ba2e907c447d8635d3854
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-11-07 11:00:11 -0300

    Update ManagerUsers.py

--- Capitulo4-Dicionarios/ManagerUsers.py
+++ Capitulo4-Dicionarios/ManagerUsers.py
@@ -1,20 +1,7 @@
+import Capitulo4_Dicionarios.Funcoes
 usuarios={}
-opcao=input("O que deseja realizar?" +
-            "<I> - Para Inserir um usuário"+
-            "<P> - Para Pesquisar um usuário"+
-            "<E> - Para Excluir um usuário"+
-            "<L> - Para Listar um usuário: ").upper()
+opcao=perguntar()
 while opcao=="I" or opcao=="P" or opcao=="E" or opcao=="L":
     if opcao=="I":
-        chave = input("Digite o login: ").upper()
-        usuarios[chave] = [input("Digite o nome: ").upper(),
-                           input("Digite a última data de acesso: "),
-                           input("Qual a última estação acessada: ").upper()]
-    opcao = input("O que deseja realizar?" +
-                  "<I> - Para Inserir um usuário" +
-                  "<P> - Para Pesquisar um usuário" +
-                  "<E> - Para Excluir um usuário" +
-                  "<L> - Para Listar um usuário: ").upper()
-    usuarios[input("Digite o login: ").upper()] = [input("Digite o nome: ").upper(),
-                                                   input("Digite a última data de acesso: "),
-                                                   input("Qual a última estação acessada: ").upper()]
+        inserir(usuarios)
+    opcao = perguntar()
