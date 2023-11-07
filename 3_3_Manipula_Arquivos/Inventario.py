d3367eb96d3633cae5766abab9b9375cf36a7be7
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-11-07 12:13:45 -0300

    Update Inventario.py

--- 3_3_Manipula_Arquivos/Inventario.py
+++ 3_3_Manipula_Arquivos/Inventario.py
@@ -4,10 +4,13 @@ opcao=chamarMenu()
 while opcao>0 and opcao<4:
     if opcao==1:
         registrar(inventario)
-    elif opcao==2:
-        persistir(inventario)
-    elif opcao==3:
-        resultado = exibir()
-        for linha in resultado:
-            print(linha[2:-1])
-    opcao = chamarMenu()
+for linha in resultado:
+        separacao=linha[linha.find(";")+1:-1]
+        data=separacao[0:separacao.find(";")]
+        separacao = separacao[separacao.find(";")+1:-1]
+        descricao=separacao[0:separacao.find(";")]
+        departamento=linha[linha.rfind(";")+1:-1]
+        print("Data.........: ", data)
+        print("Descrição....: ", descricao)
+        print("Departamento.: ", departamento)
+opcao = chamarMenu()
