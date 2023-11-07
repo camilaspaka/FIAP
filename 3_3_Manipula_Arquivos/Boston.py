a5b14b65869233dd905ceebdfefc25479f5b9bcd
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-11-07 12:26:11 -0300

    Create Boston.py

---
+++ 3_3_Manipula_Arquivos/Boston.py
@@ -0,0 +1,5 @@
+with open("economic-indicators.csv","r") as boston:
+    total=0
+    for linha in boston.readlines()[1:-1]:
+        total=total+float(linha.split(",")[3])
+    print("O total de voos é: ", total)
