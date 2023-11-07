59305f39dc266b8205918ee9a9f58a8161ef7e04
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-11-07 17:22:39 -0300

    Update Manipular_JSON.py

--- 3_3_Manipula_Arquivos/Manipular_JSON.py
+++ 3_3_Manipula_Arquivos/Manipular_JSON.py
@@ -1,7 +1,7 @@
 import json
-inventario={}
-opcao=int(input("Digite: "
-                      "<1> para registrar ativo"
+with open("inventario_json.json", "r") as arq_json:
+    inventario = json.load(arq_json)
+opcao=int(input("Digite: <1> para registrar ativo"
                       "<2> para exibir ativos armazenados: "))
 while opcao>0 and opcao<3:
     if opcao==1:
@@ -10,16 +10,16 @@ while opcao>0 and opcao<3:
             inventario[input("Digite o número patrimonial: ")] = [
                 input("Digite a data da última atualização: "),
                 input("Digite a descrição: "),
-                input("Digite o departamento: ")]
+		 input("Digite o departamento: ")]
             resp = input("Digite <S> para continuar.").upper()
         with open("inventario_json.json", "w") as arq_json:
             json.dump(inventario, arq_json)
         print("JSON gerado!!!!")
     elif opcao==2:
-            with open("inventario_json.json", "r") as arq_json:
-    		 resultado = json.load(arq_json)
-    		 for chave, dado in resultado.items():
-                	print("Data.........: ", dado[0])
-                	print("Descrição....: ", dado[1])
-                	print("Departamento.: ", dado[2])
-    opcao = int(input("Digite: "<1> para registrar ativo(" "  " ")<2> para exibir ativos armazenados: "))
+            for chave, dado in inventario.items():
+                print("Data.........: ", dado[0])
+                print("Descrição....: ", dado[1])
+                print("Departamento.: ", dado[2])
+    opcao = int(input("Digite: "
+                      "<1> para registrar ativo"
+                      "<2> para exibir ativos armazenados: "))
