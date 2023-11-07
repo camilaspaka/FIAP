ed65e7e0a74dd6c3f98ba105fe5050b515455882
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-11-07 11:01:30 -0300

    Update ManagerUsers.py

--- Capitulo4-Dicionarios/ManagerUsers.py
+++ Capitulo4-Dicionarios/ManagerUsers.py
@@ -1,7 +1,14 @@
 import Capitulo4_Dicionarios.Funcoes
 usuarios={}
+
 opcao=perguntar()
 while opcao=="I" or opcao=="P" or opcao=="E" or opcao=="L":
     if opcao=="I":
         inserir(usuarios)
+    if opcao=="P":
+        pesquisar(usuarios,input("Qual login deseja pesquisar? "))
+    if opcao == "E":
+        excluir(usuarios,input("Qual login deseja excluir? "))
+    if opcao == "L":
+        listar(usuarios)
     opcao = perguntar()
