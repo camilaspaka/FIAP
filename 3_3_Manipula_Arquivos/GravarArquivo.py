f54b16743bd2ef7095884069d2647db663c75bd7
Cah Spaka <55326571+camilaspaka@users.noreply.github.com>
2023-11-07 11:16:31 -0300

    manipulação de arquivos

--- 3_3_Manipula_Arquivos/GravarArquivo.py
+++ 3_3_Manipula_Arquivos/GravarArquivo.py
@@ -1,2 +1,5 @@
 with open("teste.txt", "w") as arquivo:
     arquivo.write("Nunca foi tão fácil criar um arquivo.")
+
+with open("teste.txt", "w") as arquivo:
+    arquivo.write("Continuação do texto.")
